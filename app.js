804de9a02242d537d31406da5e608858bb111223
ponyspy <desade4me@gmail.com>
2014-04-23 19:06:44 +0400

    schema

--- app.js
+++ app.js
@@ -84,7 +84,6 @@ var Test = models.Test;
 var User = models.User;
 var Course = models.Course;
 var Lesson = models.Lesson;
-var Scene = models.Scene;
 var Block = models.Block;
 var Exercise = models.Exercise;
 
--- models/main.js
+++ models/main.js
@@ -3,73 +3,69 @@ var mongoose = require('mongoose'),
 		Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-   login: String,
-password: String,
-   email: String,
-  status: {type: String, default: 'User'},
-    date: {type: Date, default: Date.now},
-    blocks: [{
-      block: { type: Schema.Types.ObjectId, ref: 'Block' },
-      progress: Number
-    }]
+		login: String,
+		password: String,
+		email: String,
+		status: {type: String, default: 'User'},
+		exercises: [{
+			exercise: { type: Schema.Types.ObjectId, ref: 'Exercise' },
+			progress: Number
+		}],
+		date: {type: Date, default: Date.now},
 });
 
 var courseSchema = new Schema({
-    title: String,
-    description: String,
-    lessons: [{ type: Schema.Types.ObjectId, ref: 'Lesson' }],
-    date: {type: Date, default: Date.now},
+		title: String,
+		description: String,
+		lessons: [{ type: Schema.Types.ObjectId, ref: 'Lesson' }],
+		date: {type: Date, default: Date.now},
 });
 
 var lessonSchema = new Schema({
-    title: String,
-    description: String,
-    scenes: [{ type: Schema.Types.ObjectId, ref: 'Scene' }],
-    blocks: [{ type: Schema.Types.ObjectId, ref: 'Block' }],
-    date: {type: Date, default: Date.now},
-});
-
-var sceneSchema = new Schema({
-    title: String,
-    description: String,
-    vocabulary: [String],
-    exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
-    date: {type: Date, default: Date.now},
+		title: String,
+		description: String,
+		scenes: [{ type: Schema.Types.ObjectId, ref: 'Block' }],
+		blocks: [{ type: Schema.Types.ObjectId, ref: 'Block' }],
+		test: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
+		examination: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
+		date: {type: Date, default: Date.now},
 });
 
 var blockSchema = new Schema({
-    category: String,
-    exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
-    static_content: [{
-      title: String,
-      content: String
-    }],
-    date: {type: Date, default: Date.now},
+		title: String,
+		description: String,
+		vocabulary: [String],
+		exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
+		static_content: [{
+			title: String,
+			content: String
+		}],
+		date: {type: Date, default: Date.now},
 });
 
 var exerciseSchema = new Schema({
-    type: {type: String, default: 'Base'},
-    task: String,
-    date: {type: Date, default: Date.now},
+		type: {type: String, default: 'Base'},
+		task: String,
+		date: {type: Date, default: Date.now},
 });
 
 
 
 var TestSchema = new Schema({
-  title: {
-    type: String,
-    i18n: true
-  },
-  description: {
-    type: String,
-    i18n: true
-  },
-  comments: {
-  	title: {
-  		type: String,
-  		i18n: true
-  	}
-  }
+	title: {
+		type: String,
+		i18n: true
+	},
+	description: {
+		type: String,
+		i18n: true
+	},
+	comments: {
+		title: {
+			type: String,
+			i18n: true
+		}
+	}
 });
 
 TestSchema.plugin(i18nMongoose, {languages: ['en', 'ru'], defaultLanguage: 'ru'});
@@ -80,6 +76,5 @@ module.exports.Test = mongoose.model('Test', TestSchema);
 module.exports.User = mongoose.model('User', userSchema);
 module.exports.Course = mongoose.model('Course', courseSchema);
 module.exports.Lesson = mongoose.model('Lesson', lessonSchema);
-module.exports.Scene = mongoose.model('Scene', sceneSchema);
 module.exports.Block = mongoose.model('Block', blockSchema);
 module.exports.Exercise = mongoose.model('Exercise', exerciseSchema);
